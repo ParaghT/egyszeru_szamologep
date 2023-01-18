9d77b32028d70064facdaae5260c7b8bbfc86b1d
ParaghT <118506519+ParaghT@users.noreply.github.com>
2023-01-18 15:09:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _11_017_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Művelet: ");
+            String muv = Console.ReadLine();
+            int szam1 = int.Parse(muv.Split()[0]);
+            int szam2 = int.Parse(muv.Split()[2]);
+            Char mt = Char.Parse(muv.Split()[1]);
+           
+            
+            switch (mt)
+            {
+                case '+':
+                    kiir(muv);
+                    break;
+                case '-':
+                    kiir(muv);
+                    break;
+                case '*':
+                    kiir(muv);
+                    break;
+                case '/':
+                    kiir(muv);
+                    break;
+                default:
+                    Console.WriteLine("Nincs ilyen művelet.");
+                    break;
+
+            }
+         
+        }
+        static void kiir(String i)
+        {
+            Console.WriteLine(i);
+        }
+            
+            
+        static int osszead(int a, int b)
+        {
+            return a + b;
+        }
+        static int kivonas(int a, int b)
+        {
+            return a - b;
+        }
+        static int szorzas(int a, int b)
+        {
+            return a * b;
+        }
+        static Double osztas(int a, int b)
+        {
+            return (float)a / b;
+        }
+    }
+}
